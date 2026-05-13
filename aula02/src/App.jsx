f0c01f4f412fa67771e91f38d6ad000d3c0818a4
João Gabriel <joao.batista128@etec.sp.gov.br>
2026-05-13 14:11:54 -0300

    13/05

--- aula02/src/App.jsx
+++ aula02/src/App.jsx
@@ -1,9 +1,17 @@
-export default function App()
+import "./App.css";
+
+export default function App ()
 {
   return (
     <div>
-      <h1>Aula 02 - Estudo de componentes e Props</h1>
 
+    <h1>Aula 02 - Estudo de Componentes e Props</h1>
+
+<div className="card">
+
+<h3>Chamada para o Exemplo 1</h3>
+
+    </div>
 
     </div>
   )
