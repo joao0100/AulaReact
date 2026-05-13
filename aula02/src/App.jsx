a3aae767b4ae9ba80fa6f31736422a6780fbc5c0
João Gabriel <joao.batista128@etec.sp.gov.br>
2026-05-13 15:01:45 -0300

    135

--- aula02/src/App.jsx
+++ aula02/src/App.jsx
@@ -1,17 +1,36 @@
 import "./App.css";
 
-export default function App ()
+import Exemplo1 from "./Paginas/Exemplo1";
+
+import Exemplo2 from "./Paginas/Exemplo2";
+
+import Exe1 from "./Paginas/Exe1.jsx";
+
+export default function App()
 {
   return (
     <div>
+      <h1>Aula 02 - Estudo de Componentes e Props</h1>
 
-    <h1>Aula 02 - Estudo de Componentes e Props</h1>
+      <div className="card">
+        <h3>Chamada para o Exemplo 1</h3>
+            <Exemplo1 numero={33} />
+            <Exemplo1 numero={43} />
+            <Exemplo1 numero={3} />
+      </div>
 
-<div className="card">
+      <div className="card">
+          <h3>Chamada para o Exemplo 2</h3>
+          <Exemplo2 numero1={33} numero2={34} />
+          <Exemplo2 numero1={20} numero2={22} />
+      </div>
 
-<h3>Chamada para o Exemplo 1</h3>
-
-    </div>
+      <div className="card">
+          <h3>Chamada para o Exemplo 3</h3>
+          <Exe1 numero={100} />
+          <Exe1 numero={0} />
+          <Exe1 numero={32} />
+      </div>
 
     </div>
   )
--- aula02/src/Paginas/Exe1.jsx
+++ aula02/src/Paginas/Exe1.jsx
@@ -1,8 +1,11 @@
-export default function Exe1()
+export default function Exe1({numero})
 {
+    let fahrenheit = numero;
+    let celsius = Number(fahrenheit - 32) * 5 / 9;
+
     return (
-        <div>
-            
+        <div> 
+            {fahrenheit}°F é igual a {celsius}°C.
         </div>
     )
 }
